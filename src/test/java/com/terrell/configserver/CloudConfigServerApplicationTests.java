3f77912a56abb976245a2e834fa0486601be80e3
drewterrell29 <drewterrell29@gmail.com>
2016-01-23 20:43:07 -0600

    Initial commit of config server

---
+++ src/test/java/com/terrell/configserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.terrell.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CloudConfigServerApplication.class)
+public class CloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
